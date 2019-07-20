94e7f6ad60598cf1850bdd89aa32f26222edf187
christopher igbojekwe <chris@christophers-MacBook-Pro.local>
2019-07-20 19:42:00 -0400

    fixed major bugs

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Credentials } from "uport-credentials";
 import bodyParser from "body-parser";
 import ngrok from "ngrok";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 //const express = require("express");
 const decodeJWT = require("did-jwt").decodeJWT;
 const transports = require("uport-transports").transport;
@@ -10,24 +10,58 @@ const message = require("uport-transports").message.util;
 
 class App extends React.Component {
 	state = {
-		credentials: ""
+		credentials: "",
+		users: "",
+		status: ""
 	};
 
-	Login() {
-		console.log("we are loggin in");
-	}
+	Login = e => {
+		e.preventDefault();
+
+		const credentials = new Credentials({
+			appName: this.state.credentials.appName,
+			did: this.state.credentials.did,
+			privateKey: this.state.credentials.privateKey
+		});
+
+		this.setState({
+			status: "logged in!"
+		});
+		// console.log(this.state.credentials);
+		// console.log(this.state.users);
+	};
+
+	createID = async e => {
+		e.preventDefault();
+
+		const credentials = await Credentials.createIdentity();
+		credentials.appName = "App Name";
+
+		this.setState({
+			credentials: credentials
+		});
+	};
 
 	async componentDidMount() {
-		this.setState({
-			credentials: await Credentials.createIdentity()
-		});
+		fetch("/users")
+			.then(res => res.json())
+			.then(users => {
+				console.log(users);
+				this.setState({ users });
+			});
 	}
 
 	render() {
 		return (
 			<div>
-				<Button onClick={this.Login}>Login</Button>
-				<div>{this.state.credentials.privateKey}</div>
+				<Button onClick={this.createID}>Create Uport ID</Button>
+				<Button onClick={this.Login}>Log into Uport</Button>
+				<ul>
+					<li>App Name : {this.state.credentials.appName}</li>
+					<li>did : {this.state.credentials.did}</li>
+					<li>Private Key : {this.state.credentials.privateKey}</li>
+					<li>Status : {this.state.status}</li>
+				</ul>
 			</div>
 		);
 	}
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,23 @@
-var express = require("express");
+const express = require("express");
 var router = express.Router();
+const bodyParser = require("body-parser");
+const ngrok = require("ngrok");
+const decodeJWT = require("did-jwt").decodeJWT;
+const { Credentials } = require("uport-credentials");
+const transports = require("uport-transports").transport;
+const message = require("uport-transports").message.util;
+
+let endpoint = "";
+const app = express();
+app.use(bodyParser.json({ type: "*/*" }));
+
+//setup Credentials object with newly created application identity.
+const credentials = new Credentials({
+	appName: "Login Example",
+	did: "did:ethr:0x31486054a6ad2c0b685cd89ce0ba018e210d504e",
+	privateKey:
+		"ef6a01d0d98ba08bd23ee8b0c650076c65d629560940de9935d0f46f00679e01"
+});
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
@@ -7,16 +25,26 @@ router.get("/", function(req, res, next) {
 	//res.send('respond with a resource');
 
 	// And insert something like this instead:
-	res.json([
-		{
-			id: 1,
-			username: "User1"
-		},
-		{
-			id: 2,
-			username: "user2"
-		}
-	]);
+	credentials
+		.createDisclosureRequest({
+			requested: ["name"],
+			notifications: true,
+			callbackUrl: endpoint + "/callback"
+		})
+		.then(requestToken => {
+			console.log(decodeJWT(requestToken)); //log request token to console
+			const uri = message.paramsToQueryString(
+				message.messageToURI(requestToken),
+				{ callback_type: "post" }
+			);
+			const qr = transports.ui.getImageDataURI(uri);
+			res.json([
+				{
+					id: 1,
+					username: requestToken
+				}
+			]);
+		});
 });
 
 module.exports = router;
